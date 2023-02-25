c864741f90a00f2e781e3b1b0e9758aea49ce210
Inkona <101034877+Inkona@users.noreply.github.com>
2023-02-25 14:33:30 -0600

    ree

---
+++ p1.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <fstream>
+
+int main(int argc, char *argv[])
+{
+    std::ifstream file1;
+    file1.open(argv[1]);
+    std::string f1l1;
+    std::getline(file1, f1l1);
+    
+
+
+
+
+
+
+
+} 
+
