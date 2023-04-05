71a34fde6947ac0e7c31c1134d98fb6dded3f40f
Inkona <101034877+Inkona@users.noreply.github.com>
2023-04-04 23:35:25 -0500

    ree

--- P4/p4.cpp
+++ P4/p4.cpp
@@ -1,10 +1,11 @@
-//file input output
-//getline() read from file
-//store in list or linked list
-
 //probe getline to get singular word
 //run singular word through correct hashing algo
 //place word into hashtable
+
+//fix word read in 
+//count collisions
+//print table and values
+
 
 #include <iostream>
 #include <fstream>
@@ -33,49 +34,134 @@ unsigned long djb2(string str) {
 }
 
 class HashTable {
-   list<string> hashList;
-   
-   void map();
+   vector<string> table;
+   int capacity;
 
-   void dhMap();
+public: 
+   int collisions = 0;
+   int uniqueWords = 0;
+   //
+   HashTable(int X);
+   //
+   void insertLP(string str);
+   void insertQP(string str);
+   void insertDH(string str, int dhk);
 
-   
-
+   void searchLP(string str);
+   void searchQP(string str);
+   void searchDH(string str, int dhk);
 };
+
+HashTable::HashTable(int tableSize)
+{
+   int size = tableSize;
+   this->capacity = size;
+   table.resize(capacity); 
+   fill(table.begin(), table.end(), " ");
+}
+
+void HashTable::insertLP(string str)
+{
+   int locCol = 0;
+   int index = 0;
+   for(int i = 0; i < (int) table.size(); i++)
+   {
+      index = (djb2(str) + i) % capacity;
+      if(table.at(index) == " ")
+      {
+         table.at(index) = str;
+      }
+      locCol = i;
+   }
+   collisions += locCol;
+}
+
+void HashTable::insertQP(string str)
+{
+   int locCol = 0;
+   int index = 0;
+   for(int i = 0; i < (int) table.size(); i++)
+   {
+      index = (djb2(str) + i * i) % capacity;
+      if(table.at(index) == " ")
+      {
+         table.at(index) = str;
+      }
+      else if(table.at(index) == str)
+      {
+
+      }
+      locCol = i;
+   }
+   collisions += locCol;
+}
+
+void HashTable::insertDH(string str, int dhk)
+{
+   int locCol = 0;
+   int index = 0;
+   for(int i = 0; i < (int) table.size(); i++)
+   {
+      index = (djb2(str) + i * (dhk - (djb2(str) % dhk))) % capacity;
+      if(table.at(index) == " ")
+      {
+         table.at(index) = str;
+      }
+      locCol = i;
+   }
+   collisions += locCol;
+}
 
 int main(int argc, char **argv) {
    ifstream f1(argv[0]), f2(argv[1]);
    int tableSize = atoi(argv[2]);
    string lineRead;
    string singleWord;
+   int count = 0;
+   int dhk = 0;
+   HashTable myTable(tableSize);
+   map<string, list<int>> myMap;
    if(argc != 4)
    {
-      int dhk = atoi(argv[4]);
+      dhk = atoi(argv[4]);
    }
    
+   //DOESNT ENTER HERE
    while(!feof)
    {
       getline(f1, lineRead);
-      sscanf(lineRead, "%s", singleWord);
-
-
-
-      if(argv[3] == "lp")
+ 
+      istringstream instream(lineRead);
+      while(instream >> singleWord)
       {
-         
-      }
-      else if(argv[3] == "qp")
-      {
+         //if(singleWord != ())
+         cout << singleWord << " - GOT WORD" << endl;
 
-      }
-      else if(argv[3] == "dh")
-      {
-
-      }
-      else
-      {
-         cout << "FAILURE: IMPROPER COLLISION RESOLUTION STRATEGY" << endl;
+         if(argv[3] == "lp")
+         {
+            myTable.insertLP(singleWord);
+            count++;
+            cout << "RAN LP" << endl;
+         }
+         else if(argv[3] == "qp")
+         {
+            myTable.insertQP(singleWord);
+            count++;
+            cout << "RAN QP" << endl;
+         }
+         else if(argv[3] == "dh")
+         {
+            myTable.insertDH(singleWord, dhk);
+            count++;
+            cout << "RAN DH" << endl;
+         }
+         else
+         {
+            cout << "FAILURE: IMPROPER COLLISION RESOLUTION STRATEGY" << endl;
+         }
       }
    }
+   cout << "The number of words found in the file was " << count << endl;
+   cout << myTable.collisions << endl;
    return 0;
 }
